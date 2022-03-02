6dba2ad8c93f74001fb6e5f98f680eda61700921
Omar Mohamed <mourra24@gmail.com>
2022-03-02 17:27:12 +0200

    Update game.js
    added the game function to simulate a rock papper scissor game when called

--- game.js
+++ game.js
@@ -1,14 +1,48 @@
 function computerPlay() {
-    
-    let a=  Math.floor(Math.random() * 3)
-    if (a == 0){
+
+    let a = Math.floor(Math.random() * 3)
+    if (a == 0) {
         a = "rock";
     }
-    else if (a == 1){
+    else if (a == 1) {
         a = "paper";
     }
-    else{
+    else {
         a = "scissors";
     }
     return a;
+}
+
+function game(playermove, computermove) {
+    playermove = "rock"
+    computermove = computerPlay()
+    console.log(playermove)
+    console.log(computermove)
+    if (playermove == computermove) {
+        return "tie";
+    }
+    else if (playermove == "rock") {
+        if (computermove == "paper") {
+            return "lose"
+        }
+        else {
+            return "win"
+        }
+    }
+    else if (playermove == "paper") {
+        if (computermove == "scissors") {
+            return "lose"
+        }
+        else {
+            return "win"
+        }
+    }
+    else if (playermove == "scissors") {
+        if (computermove == "rock") {
+            return "lose"
+        }
+        else {
+            return "win"
+        }
+    }
 }
